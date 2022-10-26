70d0a49049fd722aa365447f52becfeb75945f8c
g.avgeris <bitkanena1>
2022-10-26 09:32:35 +0300

    - tweak image size

--- js/img_fix_height.js
+++ js/img_fix_height.js
@@ -1,18 +1,18 @@
-    jQuery( document ).ready(function() {
+jQuery( document ).ready(function() {
 
-        jQuery('#paper img').each(function() {
-            var h = jQuery(this).height();
-            var step = 45;
-            // console.log(h);
-            newheight = Math.floor(h/step);
-            if (newheight > 0) {
-                jQuery(this).height(newheight * (step+1) - 5);
-                jQuery(this).width('auto');
-                //console.log('newheight:' + (newheight * (step+1) - 5));
-            } else {
-                jQuery(this).height(step);
-                jQuery(this).width('auto');
-                // console.log('newheight:' + newheight);
-            }
-        })
-    });
+    jQuery('#paper img').each(function() {
+        var h = jQuery(this).height();
+        var step = 45;
+        // console.log('Original height:' +h);
+        newheight = Math.floor(h/step);
+        if (newheight > 0) {
+            jQuery(this).css('height', (newheight+1) * step);
+            jQuery(this).css('width','auto');
+            // console.log('newheight:' + (newheight+1) * step);
+        } else {
+            jQuery(this).height(step);
+            jQuery(this).width('auto');
+            // console.log('newheight:' + newheight);
+        }
+    })
+});
